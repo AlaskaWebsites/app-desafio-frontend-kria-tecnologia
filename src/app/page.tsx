60ffc872129fcff336d1472e99d4faeb20809c01
Danilo <alaskawebsites94@gmail.com>
2025-01-23 07:31:59 -0300

    Subindo att

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="flex items-center justify-center min-h-[90vh] bg-gradient-to-br from-gray-800 via-gray-900 to-gray-950">
-      <div className="text-center p-8 bg-gray-100 shadow-xl rounded-xl max-w-lg">
+      <div className="text-center p-8 bg-gray-100 shadow-xl rounded-xl max-w-lg mx-4 sm:mx-auto">
         <h1 className="text-4xl font-bold text-gray-800 mb-6">Bem-vindo! 👋</h1>
         <p className="text-lg text-gray-700">
           Explore nosso site e descubra tudo o que preparamos para você. 😊
